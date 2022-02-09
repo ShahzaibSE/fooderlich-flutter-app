7ec4ab47e1792d9f4756cbc1d6b2db4ffe403720
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2022-02-09 22:14:31 +0500

    Updated

---
+++ lib/home.widget.dart
@@ -0,0 +1,68 @@
+import "package:flutter/material.dart";
+import "./fooderlich.theme.dart";
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final theme = FooderlichTheme.dark();
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Fooderlich',
+          style: theme.textTheme.headline6,
+        ),
+      ),
+      body: Center(
+        child: Text(
+          'Let\'s get cooking 👩‍🍳',
+          // 5
+          style: theme.textTheme.headline1,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        // 5
+        selectedItemColor: theme.textSelectionTheme.selectionColor,
+        // TODO: Set selected tab bar
+        // 6
+        items: <BottomNavigationBarItem>[
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card2',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.card_giftcard),
+            label: 'Card3',
+          ),
+        ],
+      ),
+    );
+  }
+}
